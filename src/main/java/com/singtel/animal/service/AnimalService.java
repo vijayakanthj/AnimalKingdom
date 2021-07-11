ea764e10c341bf0f2f023c7ccbad7e86ed9d85ef
vijay <vijayakanthj@gmail.com>
2021-07-11 18:43:37 +0800

    Animal service added

---
+++ src/main/java/com/singtel/animal/service/AnimalService.java
@@ -0,0 +1,53 @@
+package com.singtel.animal.service;
+
+import com.singtel.animal.actions.AnimalActivity;
+import com.singtel.animal.model.*;
+import org.springframework.stereotype.Service;
+
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AnimalService {
+
+    private List<Animal> animals;
+
+    @PostConstruct
+    void init() {
+        animals = new ArrayList<>();
+        Animal[] animalTypes = new Animal[]{
+                new Cat(),
+                new Dog(),
+                new Butterfly(),
+                new Caterpillar(),
+                new Chicken(),
+                new Rooster(),
+                new ClownFish(),
+                new Dolphin(),
+                new Duck(),
+                new Parrot(new Dog()),
+                new Parrot(new Cat()),
+                new Parrot(new Rooster()),
+                new Shark()
+        };
+        animals.addAll(Arrays.asList(animalTypes));
+    }
+
+    public int count(Class<? extends AnimalActivity> activity) {
+        return (int) animals.stream().filter(activity::isInstance).count();
+    }
+
+    public List<String> listAnimals(Class<? extends AnimalActivity> activity) {
+        return animals.stream().filter(activity::isInstance).map(Animal::getName)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> listAnimals() {
+        return animals.stream().map(Animal::getName).collect(Collectors.toList());
+    }
+
+}
